a5283249cb8d34055c15f1fc93edf607a6f28c14
Jasil Shan <jasilshan786@gmail.com>
2023-12-20 01:16:27 +0530

    Schema created

---
+++ server/model/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: [true, "Your email address is required"],
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Your username is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Your password is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+  });
+
+  const userModel = mongoose.model("user", userSchema)
+
+export default userModel
