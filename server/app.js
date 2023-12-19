2113573de0634f3da005f12071e4a5a8d436966b
Jasil Shan <jasilshan786@gmail.com>
2023-12-20 00:55:15 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
 import express from "express"
 import dbConnect from "./config/dbConfig.js"
-
+import cors from 'cors'
+import 'dotenv/config.js'
 
 const app = express()
 
 
 dbConnect()
 
+app.use(
+    cors({
+    }))
+app.use(express.json())
 
-const PORT = 3000
+const {PORT} = process.env
 
 app.listen(PORT, () => {
-    console.log('Server listening on:' + PORT);
+    console.log(`Server listening on: ${PORT}`);
 })
+
