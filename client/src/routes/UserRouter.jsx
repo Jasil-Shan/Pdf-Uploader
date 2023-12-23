3d8638968253565c5a41665874ad4330702f0f3c
Jasil Shan <jasilshan786@gmail.com>
2023-12-24 00:55:00 +0530

    protected route added

--- client/src/routes/UserRouter.jsx
+++ client/src/routes/UserRouter.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Signup from '../components/Signup/Signup'
 import Login from '../components/Login/Login'
+import Home from '../components/Home/Home'
+import ProtectedRoutes from '../utils/ProtectedRoutes'
 
 
 
@@ -11,6 +13,9 @@ const UserRouter = () => {
         <Routes >
             <Route path='/signup' element={<Signup />} />
             <Route path='/login' element={<Login />} />
+            <Route element = {<ProtectedRoutes />}>
+            <Route path='/' element={<Home />} />
+            </Route>
         </Routes>
     )
 }
