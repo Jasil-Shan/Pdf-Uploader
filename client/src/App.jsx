acab72d5947938f46598e6bd88a363c283bffe80
Jasil Shan <jasilshan786@gmail.com>
2023-12-25 14:25:36 +0530

    bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UserRouter from './routes/userRouter'
 import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import UserRouter from './routes/UserRouter';
 
 function App() {
   axios.defaults.baseURL = "http://localhost:4000"
