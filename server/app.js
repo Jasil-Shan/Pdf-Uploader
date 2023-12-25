6f21d49e95bece8a4645ffd404ed89bd2902c758
Jasil Shan <jasilshan786@gmail.com>
2023-12-25 15:54:09 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -19,9 +19,9 @@ app.use(
         credentials: true,
     }))
 
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "150mb" }))
 app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({limit: '150mb', extended: true }))
 
 app.use('/', userRouter)
 
